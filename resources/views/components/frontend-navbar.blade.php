64ea95c0a41cfd242dfaebec3201a7fe2ead6cd5
TAMANGALIR <tamangalir@gmail.com>
2025-07-27 17:16:08 +0545

    Frontend

--- resources/views/components/frontend-navbar.blade.php
+++ resources/views/components/frontend-navbar.blade.php
@@ -1,14 +1,7 @@
-<nav class="container flex justify-between items-center py-4">
-    <!-- Company Logo -->
+<nav class="container flex justify-between items-center">
     <div>
-        @if (!empty($company) && $company->logo)
-            <img src="{{ asset(Storage::url($company->logo)) }}" class="h-[40px]" alt="{{ $company->name }}">
-        @else
-            <img src="{{ asset('images/default-logo.png') }}" class="h-[40px]" alt="Default Company">
-        @endif
+        <img src="{{ asset(Storage::url($company->logo)) }}" class="h-[40px]" alt="{{ $company->name }}">
     </div>
-
-    <!-- Search Bar -->
     <form class="w-[40%] mx-auto">
         <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
         <div class="relative">
@@ -26,10 +19,35 @@
                 class="text-white absolute end-2.5 bottom-2.5 bg-[var(--color-primary)] hover:bg-[var(--color-primary)] focus:ring-4 focus:outline-none focus:ring-[var(--color-primary)] font-medium rounded-lg text-sm px-4 py-2">Search</button>
         </div>
     </form>
+    <div>
 
-    <!-- Auth Links -->
-    <div class="space-x-4">
-        <a href="#" class="text-sm font-medium text-gray-700 hover:text-[var(--color-primary)]">Sign In</a>
-        <a href="#" class="text-sm font-medium text-gray-700 hover:text-[var(--color-primary)]">Sign Up</a>
+        <button id="loginDropdown" data-dropdown-toggle="dropdown"
+            class="border border-white px-4 py-1 flex items-center rounded-2xl"
+            type="button">Login <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="m1 1 4 4 4-4" />
+            </svg>
+        </button>
+
+        <!-- Dropdown menu -->
+        <div id="dropdown"
+            class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44">
+            <ul class="py-2 text-sm text-gray-700" aria-labelledby="loginDropdown">
+                <li>
+                    <a href="/admin"
+                        class="block px-4 py-2 hover:bg-gray-100">Admin</a>
+                </li>
+                <li>
+                    <a href="/shop"
+                        class="block px-4 py-2 hover:bg-gray-100">Shop</a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="block px-4 py-2 hover:bg-gray-100">User</a>
+                </li>
+            </ul>
+        </div>
+
     </div>
 </nav>
